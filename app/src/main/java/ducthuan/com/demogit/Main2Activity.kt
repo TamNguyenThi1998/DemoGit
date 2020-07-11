988dfc6067a21377a41fc30fa2c9a244eea83f7a
TamNguyenThi1998 <38530749+TamNguyenThi1998@users.noreply.github.com>
2020-07-11 16:20:12 +0700

    Update Main2Activity.kt
    anh xa button

--- app/src/main/java/ducthuan/com/demogit/Main2Activity.kt
+++ app/src/main/java/ducthuan/com/demogit/Main2Activity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Main2Activity : AppCompatActivity() {
+    
+    Button btn;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
